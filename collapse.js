d313532bc0499944bc95e49fe52f9a70b64be5af
Brian Steffens <briansteffens@gmail.com>
2016-04-05 20:41:47 -0400

    Clean up

--- collapse.js
+++ collapse.js
@@ -3,52 +3,37 @@
   for (let i = 0; i < comments.length; i++) {
     let comment = comments[i];
 
-    let indent_level = comment.querySelector('td.ind img');
-    if (indent_level === null) {
+    // Skip OP
+    if (comment.querySelector('td.ind img') === null) {
       continue;
     }
-
-    let comment_head = comment.querySelector('.comhead');
-    let username = comment.querySelector('.comhead a');
 
     let collapse = document.createElement('a');
     collapse.style.cursor = 'pointer';
     collapse.style.marginRight = 5;
+    collapse.style.fontFamily = 'monospace';
     collapse.appendChild(document.createTextNode('[-]'));
-    comment_head.insertBefore(collapse, username);
 
     collapse.onclick = function() {
-      let op_indent = null;
-      let new_display = '';
+      let text = comment.querySelector('.comment');
 
-      for (let j = 0; j < comments.length; j++) {
-        let indent = comments[j].querySelector('td.ind img');
+      // Toggle display
+      text.style.display = text.style.display === '' ? 'none' : '';
+      collapse.innerHTML = text.style.display === '' ? '[-]' : '[+]';
+      let origin_indent = comment.querySelector('td.ind img').width;
 
-        if (indent === null) {
-          continue;
-        }
-
-        if (comments[j] === comment) {
-          let text = comments[j].querySelector('.comment');
-          if (text.style.display === '') {
-            new_display = 'none';
-          }
-          text.style.display = new_display;
-          collapse.innerHTML = new_display === '' ? '[-]' : '[+]';
-          op_indent = indent.width;
-          continue;
-        }
-
-        if (op_indent === null) {
-          continue;
-        }
-
-        if (indent.width <= op_indent) {
+      for (let j = i + 1; j < comments.length; j++) {
+        // Loop until the end of sub-comments
+        if (comments[j].querySelector('td.ind img').width <= origin_indent) {
           break;
         }
 
-        comments[j].style.display = new_display;
+        comments[j].style.display = text.style.display;
       }
     };
+
+    let comment_head = comment.querySelector('.comhead');
+    let username = comment.querySelector('.comhead a');
+    comment_head.insertBefore(collapse, username);
   }
 })();
